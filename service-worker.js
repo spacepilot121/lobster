9dc17a292e886cf5e8f434253d4d41ee45caab9d
spacepilot121 <markbassett1@gmail.com>
2026-07-01 20:56:32 +0100

    Unlock moult tab at full growth

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='thousand-year-tank-v1';
+const CACHE='thousand-year-tank-v2';
 const ASSETS=['./','./index.html','./styles.css','./manifest.json','./src/main.js','./src/config.js','./src/state.js','./src/upgrades.js','./src/rituals.js','./src/achievements.js','./src/save.js','./src/render.js','./src/visuals.js','./src/utils.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
